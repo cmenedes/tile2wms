46c1c3641c6530b045dfe05be413d2357d719556
cmenedes <cmenedes@yahoo.com>
2019-05-10 16:53:18 -0400

    Add dotenv and other tile formats for param

--- tms.js
+++ tms.js
@@ -1,11 +1,18 @@
 const tilegrid = require('./ol/tilegrid').createXYZ()
 
+const formats = {
+  png: 'image/png',
+  pbf: 'application/x-protobuf;type=mapbox-vector'
+}
+
 module.exports = (request, response) => {
   const params = request.params
   const extent = tilegrid.getTileCoordExtent([params.z, params.x, params.y])
   let url = process.env.WMS_TEMPLATE_URL
   url += `&layers=${params.layer}`
   url += `&bbox=${extent.join(',')}`
-  url += `&format=${params.format}`
-  response.send(url)
+  url += `&format=${formats[params.format]}`
+
+  console.warn(url)
+  response.redirect(url)
 }
