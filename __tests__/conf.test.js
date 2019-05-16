a34bb8dc970d19009ab0de956139e4cb7fc59605
timkeane <tkeane@doitt.nyc.gov>
2019-05-16 09:06:54 -0400

    add not equal assertion

--- __tests__/conf.test.js
+++ __tests__/conf.test.js
@@ -18,8 +18,9 @@ test('conf location in code', () => {
 })
 
 test('conf location in env', () => {
-  expect.assertions(1)
+  expect.assertions(2)
   process.env['CONF'] = testFile
   const conf = require('../src/conf')
+  expect(conf).not.toEqual(defaultConf)
   expect(conf).toEqual(testConf)
 })
